709d46b6cf86ba51f60f82d67f182169d23721ba
Dmitriy Dakhnovskiy <dmitriy_dakhnovskiy@mail.ru>
2017-10-07 15:40:20 +0300

    # Класс запроса

---
+++ request/request.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Dmitriy.Dakhnovskiy'
+
+from urllib.parse import parse_qs
+
+
+class Request:
+
+    @property
+    def query_params(self):
+        """
+        Параметры запроса
+        Загружается один раз при первом обращении
+        :return: словарь параметров запроса
+        """
+        if self.__query_params is None:
+            self.__query_params = parse_qs(self.__environ.get('QUERY_STRING'))
+        return self.__query_params
+
+    def __init__(self, environ):
+        """
+        :param environ: словарь - окружение приходящее в application
+        """
+        self.__environ = environ
+        self.__query_params = None
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def __getattr__(self, item):
+        return self.__environ[item.upper()]
