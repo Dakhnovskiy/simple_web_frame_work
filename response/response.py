fdcc9cb5845542192e8ead2e2fc6978e115a28bd
Dmitriy Dakhnovskiy <dmitriy_dakhnovskiy@mail.ru>
2017-10-07 17:40:35 +0300

    + свойства по умолчанию

--- response/response.py
+++ response/response.py
@@ -3,6 +3,11 @@ __author__ = 'Dmitriy.Dakhnovskiy'
 
 
 class Response:
+    """
+    Класс ответа
+    """
+    __default_status = '200 OK'
+    __empty_response = b''
 
     def __init__(self, response=None, status=None, headers=None):
         """
@@ -10,9 +15,9 @@ class Response:
         :param status: статус ответа
         :param headers: заголовки ответа
         """
-        self.__response = response
-        self.__status = status
-        self.__headers = headers
+        self.__response = self.__empty_response if response is None else response
+        self.__status = self.__default_status if status is None else status
+        self.__headers = [] if headers is None else headers
 
     def __call__(self, start_response):
         """
